1371384d4aa05d985b1cbec2d6ea5ae6e29347da
ziyang-666 <84216713+ziyang-666@users.noreply.github.com>
2021-06-23 23:48:43 -0400

    Add more tester

--- HashMap/src/MyHashMapTest.java
+++ HashMap/src/MyHashMapTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 class MyHashMapTest {
 
 	@Test
-	public void MyHashMap_Size() {
+	public void MyHashMap_size() {
 		MyHashMap<String, Integer> map = new MyHashMap<>();
 		assertEquals(0, map.size(), 0.00001);
 		map.put("a", 1);
@@ -14,11 +14,51 @@ class MyHashMapTest {
 	}
 	
 	@Test
-	public void MyHashMap_Empty() {
+	public void MyHashMap_empty() {
 		MyHashMap<String, Integer> map = new MyHashMap<>();
 		assertEquals(true, map.isEmpty());
 		map.put("a", 1);
 		map.put("b", 1);
 		assertEquals(false, map.isEmpty());
+	}
+	
+	@Test
+	public void MyHashMap_put() {
+		MyHashMap<String, Integer> map = new MyHashMap<>();
+		assertEquals(null, map.get("a"));
+		map.put("a", 1);
+		map.put("b", 1);
+		assertEquals(1, map.get("a"), 0.00001);
+		map.put("a", 2);
+		assertEquals(2, map.get("a"), 0.00001);
+	}
+	
+	@Test
+	public void MyHashMap_get() {
+		MyHashMap<String, Integer> map = new MyHashMap<>();
+		assertEquals(null, map.get("a"));
+		map.put("a", 1);
+		map.put("b", 1);
+		assertEquals(1, map.get("a"), 0.00001);
+		map.put("a", 2);
+		assertEquals(2, map.get("a"), 0.00001);
+	}
+	
+	@Test
+	public void MyHashMap_containsValue() {
+		MyHashMap<String, Integer> map = new MyHashMap<>();
+		assertEquals(false, map.containsValue(58));
+		map.put("abcdf", 58);
+		assertEquals(true, map.containsValue(58));
+		map.put("abcdf", 238238);
+		assertEquals(false, map.containsValue(58));
+	}
+	
+	@Test
+	public void MyHashMap_containsKey() {
+		MyHashMap<String, Integer> map = new MyHashMap<>();
+		assertEquals(false, map.containsKey("sdisjdsidj"));
+		map.put("sdisjdsidj", 9807);
+		assertEquals(true, map.containsKey("sdisjdsidj"));
 	}
 }
